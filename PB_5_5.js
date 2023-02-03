c3f669c430bc649229480e2fbd6d006cc5d5efdc
Tsvetelin Dragoev <119632196+ilarius3@users.noreply.github.com>
2023-02-03 13:40:55 +0400

    Update PB_5_5.js

--- PB_5_5.js
+++ PB_5_5.js
@@ -11,8 +11,7 @@ function read (input) {
         console.log(`Increase: ${count.toFixed(2)}`);
         count2 += Number(sum);    
         sum = input[index];
-        index++;
-    }
+        index++;}
     console.log(`Total: ${count2.toFixed(2)}`);
 } read (["5.51", 
 "69.42",
