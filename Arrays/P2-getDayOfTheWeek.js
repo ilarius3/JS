4a9c80c796e109f729af659565cd0c4b560c4552
Tsvetelin Dragoev <119632196+ilarius3@users.noreply.github.com>
2023-02-11 22:27:00 +0100

    Create P2-getDayOfTheWeek.js

---
+++ Arrays/P2-getDayOfTheWeek.js
@@ -0,0 +1,20 @@
+function getDayOfTheWeek(num) {
+    let daysOfTheWeek = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'    
+    ]
+    if (num < 1 || num > 7) {
+        console.log('Invalid day!');
+    } else {
+        console.log(daysOfTheWeek[num - 1])
+    }
+    
+}
+getDayOfTheWeek(3);
+getDayOfTheWeek(6);
+getDayOfTheWeek(11);
