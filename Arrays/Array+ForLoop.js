e99afc8564b937ddfa0a2ea5b9f566c7ebe7e616
Tsvetelin Dragoev <119632196+ilarius3@users.noreply.github.com>
2023-02-12 23:35:32 +0100

    Update Array+ForLoop.js

--- Arrays/Array+ForLoop.js
+++ Arrays/Array+ForLoop.js
@@ -1,4 +1,5 @@
-let numbers = [4, 8, 13, 24]
-for (let i = 0; i <= 3; i++) {
+let numbers = [4, 8, 13, 24, 48]
+for (let i = 0; i <= numbers.length-1; i++) {
     console.log(numbers[i])
 }
+console.log(numbers.length)
