a4a4a01dd6afe1644b5828928a0a09501f599d21
Tsvetelin Dragoev <119632196+ilarius3@users.noreply.github.com>
2023-02-23 00:15:07 +0100

    Create 7-ListOfProducts.js

---
+++ Arrays/7-ListOfProducts.js
@@ -0,0 +1,7 @@
+function sorted(arr) {
+arr.sort((a, b) => a.localeCompare(b));
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`${i+1}.${arr[i]}`)
+    }
+}
+sorted(['Potatoes', 'Tomatoes', 'Onions', 'Apples']);
