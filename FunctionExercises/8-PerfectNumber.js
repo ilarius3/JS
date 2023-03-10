9655d58de929e50a0d9b5a741412a6434448081e
Tsvetelin Dragoev <119632196+ilarius3@users.noreply.github.com>
2023-03-10 21:34:41 +0100

    Create 8-PerfectNumber.js

---
+++ FunctionExercises/8-PerfectNumber.js
@@ -0,0 +1,14 @@
+function perfectNumber(num){	//Задача 5:   
+    let sum = 0;
+    for (let i = 1; i < num; i++) {
+        if (num % i === 0){ 
+            sum += i;
+        }
+    }
+    if (num === sum){
+        console.log("We have a perfect number!")
+    } else {
+        console.log("It's not so perfect.")  
+    }
+}   
+perfectNumber(28)
