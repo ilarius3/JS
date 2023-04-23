e9d7811aa1d98c4715123c181944880981efb4a6
Tsvetelin Dragoev <119632196+ilarius3@users.noreply.github.com>
2023-04-23 23:43:09 +0200

    Create 1-Laptop.js

---
+++ ObjetAndClassesMoreExercises/1-Laptop.js
@@ -0,0 +1,38 @@
+class Laptop {
+    constructor(info, quality) {
+        this.info = {
+            producer: info.producer,
+            age: info.age,
+            brand: info.brand,
+        }
+        this.quality = quality;
+        this.isOn = false;
+    }
+    turnOn() {
+        this.isOn = true;
+        if (this.quality > 0) {
+            this.quality -= 1;
+        }
+    }
+    turnOff() {
+        this.isOn = false;
+        if (this.quality > 0) {
+            this.quality -= 1;
+        }
+    }
+    showInfo() {
+        return JSON.stringify(this.info);
+    }
+    get price() {
+        return 800 - (this.info.age * 2) + (this.quality * 0.5);
+    }
+}
+let info = {producer: "Dell", age: 2, brand: "XPS"};
+let laptop = new Laptop(info, 10);
+laptop.turnOn();
+console.log(laptop.showInfo());
+laptop.turnOff();
+console.log(laptop.quality);
+laptop.turnOn();
+console.log(laptop.isOn);
+console.log(laptop.price);
