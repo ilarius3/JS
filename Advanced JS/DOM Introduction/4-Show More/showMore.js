44c6c128e1f04c6b8cb128340f7a82b6489d8d1b
Tsvetelin Dragoev <119632196+ilarius3@users.noreply.github.com>
2023-06-12 10:48:47 +0200

    Create showMore.js

---
+++ Advanced JS/DOM Introduction/4-Show More/showMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let span = document.querySelector('#text');
+    let anchor = document.querySelector('#more');
+    span.style.display = 'inline';
+    anchor.style.display = 'none';
+}
